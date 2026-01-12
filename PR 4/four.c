f0c6d45e5514a1df89395c1b15053184a8cd75a0
Tanmay patil <tanmaycpatil1028@gmail.com>
2026-01-12 12:59:30 +0530

    Add files via upload

---
+++ PR 4/four.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	int i,j;
+	for(i=5;i>=1;i--)
+	{
+		for(j=i;j<5;j++)
+		{
+			printf("  ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			if(j%2==0)
+			{
+				printf("0 ");
+			}
+			else
+			{
+				printf("1 ");
+			}
+		}
+		printf("\n");
+	}
+}
---
+++ PR 4/seven.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(j==1||i==1||i==3||(j==5&&i==2))
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+}
